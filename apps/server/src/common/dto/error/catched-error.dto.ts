e3dc30777eba7cc32e8fee7f01858b6e273c510c
Ryan-TheLion <dae7249@nate.com>
2025-08-08 18:18:15 +0900

    refactor: catched error DTO
    DTO 필드를 제외한 생성자와 static 메소드 제거

--- apps/server/src/common/dto/error/catched-error.dto.ts
+++ apps/server/src/common/dto/error/catched-error.dto.ts
@@ -6,25 +6,4 @@ export class CatchedErrorResponseDto {
   path: string;
 
   timestamp: string;
-
-  protected constructor(dto: {
-    name: string;
-    message: string;
-    path: string;
-    timestamp: string;
-  }) {
-    this.name = dto.name;
-    this.message = dto.message;
-    this.path = dto.path;
-    this.timestamp = dto.timestamp;
-  }
-
-  protected static of(dto: {
-    name: string;
-    message: string;
-    path: string;
-    timestamp: string;
-  }) {
-    return new CatchedErrorResponseDto(dto);
-  }
 }
