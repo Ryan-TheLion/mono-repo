eeb38ad028e9739b09e94d0f91e39d926fdcc480
Ryan-TheLion <dae7249@nate.com>
2025-08-12 07:23:41 +0900

    refactor(server): cors origin 환경변수
    - cors origin 을 좀 더 typesafe 하게 적용할 수 있도록 수정
    - origin => cors.origin
    - lint-staged --report-unused-disable-directives-severity off

--- apps/server/src/common/bootstrap.config.ts
+++ apps/server/src/common/bootstrap.config.ts
@@ -1,38 +1,79 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ConfigType, registerAs } from '@nestjs/config';
+import {
+  isArrayFormat,
+  isBooleanFormat,
+  isNumberFormat,
+  isRegExpFormat,
+} from './utils';
+
+type StaticCorsOrigin = boolean | string | RegExp | (string | RegExp)[];
 
 export type BootStrapConfig = ConfigType<typeof bootStrapConfig>;
 
-const bootStrapConfig = registerAs('bootStrap', () => {
-  const origin = (() => {
-    if (process.env.CORS_ORIGIN === undefined) return undefined;
+const bootStrapConfig = registerAs('bootStrap', async () => {
+  const origin = await loadCorsOriginsEnv();
 
-    const originEnv = new Function(`return ${process.env.CORS_ORIGIN}`)();
-
-    if (!Array.isArray(originEnv))
-      throw new Error('origin은 배열 형식으로 설정해주세요');
-
-    if (
-      !originEnv.every(
-        (value) => typeof value === 'string' || value instanceof RegExp,
-      )
-    )
-      throw new Error(
-        '개별 origin은 문자열이나 RegExp 리터럴 형식만 가능합니다',
-      );
-
-    return originEnv;
-  })();
+  const isPort = isNumberFormat(process.env.PORT ?? '');
 
   const config = {
-    port: process.env.PORT ? Number(process.env.PORT) : 8000,
-    origin,
+    port: isPort.matched ? isPort.resolvedValue : 8000,
     cors: {
+      origin,
       credentials: true,
     } satisfies CorsOptions as CorsOptions,
   };
 
   return config;
 });
+
+const loadCorsOriginsEnv = async (): Promise<StaticCorsOrigin | undefined> => {
+  const envField = process.env['CORS_ORIGINS'];
+
+  if (!envField) return undefined;
+
+  const matchedFormat = await Promise.all([
+    // eslint-disable-next-line @typescript-eslint/require-await
+    (async (format: string) => {
+      const isBoolean = isBooleanFormat(format);
+
+      if (isBoolean.matched) return isBoolean.resolvedValue;
+
+      return null;
+    })(envField),
+    // eslint-disable-next-line @typescript-eslint/require-await
+    (async (format: string) => {
+      const isRegExp = isRegExpFormat(format);
+
+      if (isRegExp.matched) return isRegExp.regexp;
+
+      return null;
+    })(envField),
+    // eslint-disable-next-line @typescript-eslint/require-await
+    (async (format: string) => {
+      const isArray = isArrayFormat(format);
+
+      if (isArray.matched) {
+        return JSON.parse(format, (key, value) => {
+          if (key === '') return value as Array<string | RegExp>;
+
+          const isRegExp = isRegExpFormat(value as string);
+
+          if (isRegExp.matched) return isRegExp.regexp;
+
+          return value as string;
+        }) as Array<string | RegExp>;
+      }
+
+      return null;
+    })(envField),
+  ]);
+
+  const matched = matchedFormat.find((value) => value != null);
+
+  if (matched) return matched;
+
+  return envField;
+};
 
 export default bootStrapConfig;
