a492f95333ef37dff0b2e037f9bf9fa9f3216765
Ryan-TheLion <dae7249@nate.com>
2025-09-05 10:02:36 +0900

    chore(server): imap config
    config module 에서 imap config를 load 하도록 설정

--- apps/server/src/common/common.module.ts
+++ apps/server/src/common/common.module.ts
@@ -4,12 +4,19 @@ import bootStrapConfig from './bootstrap.config';
 import { supabaseConfig } from 'src/supabase/supabase.config';
 import { smtpConfig } from 'src/mail/smtp/smtp.config';
 import { cloudflareR2Config } from 'src/cloudflare-r2/cloudflare-r2.config';
+import { imapConfig } from 'src/mail/imap/imap.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [bootStrapConfig, supabaseConfig, smtpConfig, cloudflareR2Config],
+      load: [
+        bootStrapConfig,
+        supabaseConfig,
+        smtpConfig,
+        imapConfig,
+        cloudflareR2Config,
+      ],
     }),
   ],
 })
